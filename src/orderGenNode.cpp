7571657469c226bf545b7e3fcd6c67ab63bf6544
Arpit Chauhan <carpit680@gmail.com>
2022-09-10 20:31:14 +0530

    Order gen node starts on button press from UI

--- src/orderGenNode.cpp
+++
@@ -1,8 +0,0 @@
-#include <OrderGen.h>
-
-int main(int argc, char * argv[]) {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<OrderGen>());
-    rclcpp::shutdown();
-    return 0;
-}
